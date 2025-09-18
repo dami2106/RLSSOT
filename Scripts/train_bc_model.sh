b4f69ebd2aca5e87f73864c9bf44be662bb490ed
dami2106 <2303289@students.wits.ac.za>
2025-09-18 15:34:32 +0200

    Add BC cnn model

--- Scripts/train_bc_model.sh
+++ Scripts/train_bc_model.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=behaviour_cloning        # Job name
+#SBATCH --job-name=behaviour_cloning_cnn        # Job name
 #SBATCH --partition=bigbatch                     # Replace with your cluster's GPU partition name
-#SBATCH --output=/home-mscluster/dharvey/HiSD/behaviour_cloning_pca512_wood.out  # Standard output and error log
+#SBATCH --output=/home-mscluster/dharvey/HiSD/behaviour_cloning_cnn_wood_2.out  # Standard output and error log
 # Load your environment
 
 source ~/.bashrc
 conda activate SOTA
 
-python RLSSOT/Skill_Learning/behavioural_cloning.py
+python Skill_Learning/behavioural_cloning_cnn.py
+# python RLSSOT/Skill_Learning/behavioural_cloning.py
