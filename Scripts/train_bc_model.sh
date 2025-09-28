d3ef0a7770fa5229ef1ca83c080394a6b9df26a0
dami2106 <2303289@students.wits.ac.za>
2025-09-28 22:02:10 +0200

    Script update

--- Scripts/train_bc_model.sh
+++ Scripts/train_bc_model.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=behaviour_cloning_cnn        # Job name
 #SBATCH --partition=bigbatch                     # Replace with your cluster's GPU partition name
-#SBATCH --output=/home-mscluster/dharvey/HiSD/behaviour_cloning_cnn_wp.out  # Standard output and error log
+#SBATCH --output=/home-mscluster/dharvey/HiSD/Experiments/behaviour_cloning_cnn_wood_tuning_noprune.out  # Standard output and error log
 # Load your environment
 
 source ~/.bashrc
 conda activate SOTA
 
-python Skill_Learning/behavioural_cloning_cnn.py --skill wood --study --trials 250 --prune
+python Skill_Learning/behavioural_cloning_cnn.py --skill wood --study --trials 250
 # python RLSSOT/Skill_Learning/behavioural_cloning.py
